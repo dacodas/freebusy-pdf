3cf3eabbbb6db2a568cc6135146e19f9c905ef55
Dacoda Strack <dacoda.strack@gmail.com>
2021-04-16 11:07:44 -0700

    allow for compatibility with C for drawDays and strokeHelper includes

--- visualize/src/drawDays.h
+++ visualize/src/drawDays.h
@@ -3,7 +3,9 @@
 #include <cairo/cairo.h>
 #include <specification.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 void draw_days(
 	cairo_t* cr,
 	const CalendarDayMargins* margins,
@@ -12,4 +14,6 @@ void draw_days(
 	const HoursContext* hoursContext,
 	const struct tm* startDate
 );
+#ifdef __cplusplus
 }
+#endif
